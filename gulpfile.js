3af7814baf941672945d6bfe5d309e97b1032c7b
周培公 <peigong@foxmail.com>
2016-05-10 20:20:57 +0800

    增加单元测试和代码覆盖率

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,41 @@ var browserSync = require('browser-sync').create();
 var src = './lib/needle.js';
 
 gulp.task('clean', function(){
-    del(['dist/**']);
+    del(['dist', 'test-tmp', 'coverage']);
 });
 
 gulp.task('static', function () {
     return gulp.src(src)
-        .pipe($.eslint())
-        .pipe($.eslint.format())
-        .pipe($.eslint.failAfterError());
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failAfterError());
 });
 
 gulp.task('build:needle', function(){
     return gulp.src(src)
-        .pipe($.uglify())
-        .pipe($.rename('n.js'))
-        .pipe(gulp.dest('./dist'));
+    .pipe($.uglify())
+    .pipe($.rename('n.js'))
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['clean', 'static', 'build:needle']);
+gulp.task('pre-test', function(){
+    return gulp.src(src)
+    .pipe($.istanbul())
+    .pipe(gulp.dest('./test-tmp'));
+});
+
+gulp.task('test', ['pre-test'], function(done){
+    return gulp.src('./test/**/*.html')
+    .pipe($.qunit())
+    .pipe($.istanbul.writeReports());
+});
+
+gulp.task('coveralls', ['test'], function(){
+    if(!process.env.CI){
+        return;
+    }
+    return gulp.src(path.join(__dirname, 'coverage/lcov.info'))
+    .pipe($.coveralls());
+});
+
+gulp.task('default', ['clean', 'static', 'build:needle', 'test', 'coveralls']);
