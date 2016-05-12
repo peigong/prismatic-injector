1f93191424f35cf5955f407c6a14c335c3ceb0bf
周培公 <peigong@foxmail.com>
2016-05-12 12:50:42 +0800

    修整构建

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# prismatic-injector  [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] #
+# prismatic-injector  [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url] #
 
 > 棱镜注射器
 
 ## 针头脚本 ##
 
-针头脚本`lib/needle.js`用于注入宿主页面，并调度后续流程。
+针头脚本用于注入宿主页面，并调度后续流程。
 
 ## 注入针头脚本 ##
 
@@ -18,10 +18,21 @@
 - `needle.js`：针头脚本文件名。实际应用中，可以使用其他名称。
 - `__pi__`：脚本元素的ID。与脚本中的全局命名空间保持同名，并全部小写。
 
-TODO:能够自定义脚本元素ID，以及全局命名空间名称。
+## 配置构建系统 ##
+
+通过`needle.json`配置构建系统，能够自定义：
+
+- 针头脚本文件名，及是否增加版本号
+- 针头脚本元素ID
+- 全局命名空间名称
+- 调度服务器
+- 调度服务名
+
+具体参见：[针头脚本配置规范(./doc/needle.md)](./doc/needle.md)
 
 ## 接口规范 ##
 
+- [针头脚本配置规范](./doc/needle.md)
 - [模板选择服务接口规范](./doc/opt.md)
 - [模板开发规范](./doc/template.md)
 
@@ -29,3 +40,5 @@ TODO:能够自定义脚本元素ID，以及全局命名空间名称。
 [travis-url]: https://travis-ci.org/peigong/prismatic-injector
 [daviddm-image]: https://david-dm.org/peigong/prismatic-injector.svg?theme=shields.io
 [daviddm-url]: https://david-dm.org/peigong/prismatic-injector
+[coveralls-image]: https://coveralls.io/repos/peigong/prismatic-injector/badge.svg
+[coveralls-url]: https://coveralls.io/r/peigong/prismatic-injector
--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,45 @@
 'use strict';
 
+var fs = require('fs');
 var del = require('del');
 var gen = require('random-gen');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync').create();
 
+// 导入配置文件
 var settings = require('./needle.json');
 if('random' === settings.namespace){ // 如果命名空间设置为'random'，则使用随机生成的标识
     settings.namespace = gen.upper(8); // 8个随机大写字符的标识
 }
 settings.id = settings.namespace.toLowerCase();
 
-var src = './lib/needle.js';
-
+// 清理构建输出
 gulp.task('clean', function(){
-    del(['dist', 'test-tmp', 'coverage']);
+    return del(['dist', '.tmp', 'coverage']);
 });
 
-gulp.task('static', function () {
-    return gulp.src(src)
+// 预处理针头脚本
+gulp.task('pre-process', function(){
+    return gulp.src('./lib/needle.js')
+    .pipe($.preprocess({ context: { AUTO: !settings.server, SERVER: settings.server } }))
+    .pipe($.replace('__PI__', settings.namespace))
+    .pipe($.replace('opt.do', settings.opt))
+    .pipe(gulp.dest('./.tmp/processed'));
+});
+
+// 代码静态检查
+gulp.task('static', ['pre-process'], function () {
+    return gulp.src('./.tmp/processed/*.js')
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.eslint.failAfterError());
 });
 
-gulp.task('build:needle', function(){
-    return gulp.src(src)
+// 构建针头脚本
+gulp.task('build:needle', ['pre-process'], function(){
+    return gulp.src('./.tmp/processed/needle.js')
     .pipe($.sourcemaps.init())
-    .pipe($.replace('__PI__', settings.namespace))
-    .pipe($.replace('opt.do', settings.opt))
     .pipe($.uglify())
     .pipe($.rename(settings.name))
     .pipe($.if(settings.revisioning, $.rev()))
@@ -39,18 +49,41 @@ gulp.task('build:needle', function(){
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('pre-test', function(){
-    return gulp.src(src)
-    .pipe($.istanbul())
-    .pipe(gulp.dest('./test-tmp'));
+// 输出针头脚本的ID和文件名
+gulp.task('build:name', ['build:needle'], function(){
+    var content = [ settings.id ];
+    if(settings.revisioning){
+
+    }else{
+        content.push(settings.name);
+    }
+    fs.writeFile('needle.name', content.join('\t:\t'));
 });
 
-gulp.task('test', ['pre-test'], function(done){
-    return gulp.src('./test/**/*.html')
+// 预处理单元测试
+gulp.task('pre-unit', function(){
+    return gulp.src('./test/**')
+    .pipe($.replace('__pi__', settings.id))
+    .pipe($.replace('opt.do', settings.opt))
+    .pipe($.replace('__PI__', settings.namespace))
+    .pipe(gulp.dest('./.tmp/unit'));
+});
+
+// 预处理待测脚本
+gulp.task('pre-test', ['pre-process'], function(){
+    return gulp.src('./.tmp/processed/needle.js')
+    .pipe($.istanbul())
+    .pipe(gulp.dest('./.tmp/test'));
+});
+
+// 执行自动化测试
+gulp.task('test', ['pre-unit', 'pre-test'], function(done){
+    return gulp.src('./.tmp/unit/**/*.html')
     .pipe($.qunit())
     .pipe($.istanbul.writeReports());
 });
 
+// 发布代码覆盖率文件
 gulp.task('coveralls', ['test'], function(){
     if(!process.env.CI){
         return;
@@ -59,4 +92,5 @@ gulp.task('coveralls', ['test'], function(){
     .pipe($.coveralls());
 });
 
-gulp.task('default', ['clean', 'static', 'build:needle', 'test', 'coveralls']);
+// 默认任务
+gulp.task('default', ['clean', 'static', 'build:needle', 'test', 'coveralls', 'build:name']);
